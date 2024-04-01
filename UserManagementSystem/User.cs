2f0cb1c59b0ba881d869118c59514959d69caaa2
Shawn Abraham <tuh11972@temple.edu>
2024-04-01 06:31:51 -0400

    GH-1 Add User Class

---
+++ UserManagementSystem/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserManagementSystem
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
